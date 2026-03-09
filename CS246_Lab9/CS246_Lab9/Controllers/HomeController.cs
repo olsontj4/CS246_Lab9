3e9ab3a8eb8721ea37c71a614cf08f443859a7c7
Thomas Olson <thomas.josholson@gmail.com>
2026-03-09 14:18:15 -0700

    Initial commit.

---
+++ CS246_Lab9/CS246_Lab9/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using CS246_Lab9.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS246_Lab9.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CS246_Lab9/CS246_Lab9/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CS246_Lab9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
