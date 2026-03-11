6c12a77e1bfb079c75c16f153aec3034bee6d8dc
InsertName-42 <justmant@my.lanecc.edu>
2026-03-11 16:16:28 -0700

    Event, Testimonial, and InvoiceLineItem

---
+++ CS246_Lab9/CS246_Lab9/Models/Testimonial.cs
@@ -0,0 +1,10 @@
+﻿namespace CS246_Lab9.Models
+{
+    public class Testimonial
+    {
+        public int Id { get; set; }
+        public required AppUser User { get; set; }
+        public required string Feedback { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
