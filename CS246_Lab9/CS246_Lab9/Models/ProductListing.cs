ef56657e22fa7ce57bc0b98a0ffae3e116ab94f8
codywatkins001 <watkinsc2@my.lanecc.edu>
2026-03-11 15:23:47 -0700

    completed AppUser, Product, and ProductListing

---
+++ CS246_Lab9/CS246_Lab9/Models/ProductListing.cs
@@ -0,0 +1,13 @@
+﻿namespace CS246_Lab9.Models
+{
+    public class ProductListing
+    {
+        public int Id { get; set; }
+        public required string ProductName { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Tags { get; set; }
+        public List<string> ImageURLs { get; set; } = new List<string>();
+        public List<string> Materials { get; set; } = new List<string>();
+        public int Quantity { get; set; }
+    }
+}
